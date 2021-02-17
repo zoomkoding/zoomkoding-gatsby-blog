3b15eb5c3b6bfbb503ef04906557eb9f525ef595
zoomkoding <zoomkoding@gmail.com>
2021-02-17 17:33:51 +0900

    feat: add style component settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-mdx`,
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
