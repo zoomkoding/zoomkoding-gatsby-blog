a430e6cbfd274e99d4544977f7794e6996d41679
zoomkoding <zoomkoding@gmail.com>
2021-02-21 16:35:03 +0900

    feat: create global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import './src/styles/reset.css';
+import './src/styles/font.css';
+import './src/styles/style.css';
+import './src/styles/markdown-style.css';
