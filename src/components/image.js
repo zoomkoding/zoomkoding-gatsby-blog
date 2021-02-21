20e22534347e57e626946a11221018ecfcf1b83f
zoomkoding <zoomkoding@gmail.com>
2021-02-22 03:26:09 +0900

    feat: create Image component

--- src/components/image.js
+++ src/components/image.js
@@ -1,36 +1,47 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React, { useMemo } from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import PropTypes from 'prop-types';
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
-const Image = () => {
+const Image = ({ src, ...rest }) => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+      images: allFile(filter: { internal: { mediaType: { regex: "/image/" } } }) {
+        edges {
+          node {
+            relativePath
+            extension
+            publicURL
+            childImageSharp {
+              fluid(maxWidth: 600) {
+                ...GatsbyImageSharpFluid
+              }
+            }
           }
         }
       }
     }
-  `)
+  `);
 
-  if (!data?.placeholderImage?.childImageSharp?.fluid) {
-    return <div>Picture not found</div>
+  const match = useMemo(() => data.images.edges.find(({ node }) => src === node.relativePath), [
+    data,
+    src,
+  ]);
+
+  if (!match) return null;
+
+  const { node: { childImageSharp, publicURL, extension } = {} } = match;
+
+  if (extension === 'svg' || !childImageSharp) {
+    return <img src={publicURL} {...rest} />;
   }
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+  return <Img fluid={childImageSharp.fluid} {...rest} />;
+};
 
-export default Image
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+};
+
+export default Image;
