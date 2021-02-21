7152e97902af38d1621a1a378c97fc876b37483a
zoomkoding <zoomkoding@gmail.com>
2021-02-22 03:33:00 +0900

    feat: add sass to gatsby config and package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Zoomkoding's Dev Log`,
+    title: `zoomkoding.dev`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `zoomkoding`,
+    social: {
+      github: `https://github.com/zoomKoding`,
+      linkedIn: `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,20 +27,17 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-mdx`,
-    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
