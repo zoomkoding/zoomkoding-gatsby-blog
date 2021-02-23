cf119c02c34688d13d268100071b1540ffd9a6b3
zoomkoding <zoomkoding@gmail.com>
2021-02-24 02:57:25 +0900

    feat: create BlogTemplate using components

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { graphql } from 'gatsby';
+import Header from '../components/header';
+import Layout from '../components/layout';
+import TableOfContents from '../components/toc';
+import PostHeader from '../components/post-header';
+import Post from '../models/post';
+import PostContent from '../components/post-content';
 
 export default ({ data }) => {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark;
+  console.log(data.markdownRemark);
+  const post = new Post(data.markdownRemark);
+  console.log(post);
 
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div className="markdown" dangerouslySetInnerHTML={{ __html: html }} />
-      </div>
-    </div>
+    <Layout>
+      <PostHeader post={post} />
+      <PostContent html={post.html} />
+      {/* <TableOfContents items={tableOfContents} currentHeaderUrl={currentHeaderUrl} /> */}
+    </Layout>
   );
 };
 
 export const pageQuery = graphql`
   query($slug: String!) {
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      id
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         slug
         title
+        categories
+        author
       }
     }
   }
