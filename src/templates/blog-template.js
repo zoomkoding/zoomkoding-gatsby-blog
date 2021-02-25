73c54391878f0b83d84e45e47b3bb853ec073eac
zoomkoding <zoomkoding@gmail.com>
2021-02-25 16:01:00 +0900

    feat: use PostCardsAdjacent in BlogTemplate

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,29 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { graphql } from 'gatsby';
-import Header from '../components/header';
 import Layout from '../components/layout';
 // import TableOfContents from '../components/toc';
 import PostHeader from '../components/post-header';
+import PostCardsAdjacent from '../components/post-cards-adjacent';
 import Post from '../models/post';
 import PostContent from '../components/post-content';
 
 export default ({ data }) => {
-  console.log(data.markdownRemark);
-  const post = new Post(data.markdownRemark);
-  console.log(post);
-
+  const curPost = new Post(data.cur);
+  const prevPost = data.prev && new Post(data.prev);
+  const nextPost = data.next && new Post(data.next);
   return (
     <Layout>
-      <PostHeader post={post} />
-      <PostContent html={post.html} />
-      {/* <TableOfContents items={tableOfContents} currentHeaderUrl={currentHeaderUrl} /> */}
+      <PostHeader post={curPost} />
+      <PostContent html={curPost.html} />
+      <PostCardsAdjacent prevPost={prevPost} nextPost={nextPost} />
     </Layout>
   );
 };
 
 export const pageQuery = graphql`
-  query($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+  query($slug: String!, $nextSlug: String!, $prevSlug: String!) {
+    cur: markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+        categories
+        author
+        emoji
+      }
+    }
+
+    next: markdownRemark(frontmatter: { slug: { eq: $nextSlug } }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+        categories
+        author
+        emoji
+      }
+    }
+
+    prev: markdownRemark(frontmatter: { slug: { eq: $prevSlug } }) {
       id
       html
       frontmatter {
