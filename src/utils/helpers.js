a9d0d54d47f0de2980243e9c151cce0046d54dfd
zoomkoding <zoomkoding@gmail.com>
2021-02-25 17:29:05 +0900

    update: update helper functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,18 @@
-export const sortObjectByValue = (obj) => {
+const sortObjectByValue = (obj) => {
   let sortable = [];
   for (let item in obj) sortable.push([item, obj[item]]);
   sortable.sort((a, b) => b[1] - a[1]);
   return sortable;
+};
+
+export const getSortedCategoriesByCount = (posts) => {
+  const cntPerCategory = {};
+
+  posts.forEach(({ categories }) => {
+    categories.forEach((category) => {
+      cntPerCategory[category] = cntPerCategory[category] ? cntPerCategory[category] + 1 : 1;
+    });
+  });
+
+  return sortObjectByValue(cntPerCategory).map(([category]) => category);
 };
