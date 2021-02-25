df51f08448b4bad80c18bf35b03228082fbb8252
zoomkoding <zoomkoding@gmail.com>
2021-02-25 22:01:18 +0900

    feat: add HorizontalTabs for responsive Posts Page

--- src/components/posts-page-body/index.js
+++ src/components/posts-page-body/index.js
@@ -2,6 +2,7 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import PostCardsColumn from '../post-cards-column';
+import Tabs from '../tabs';
 import VerticalTabs from '../vertical-tabs';
 
 import './style.scss';
@@ -10,6 +11,7 @@ const PostsPageBody = ({ tabIndex, onChange, tabs, posts }) => {
   return (
     <div className="posts-page-body-wrapper">
       <div className="posts-page-body">
+        <Tabs className={'tabs'} value={tabIndex} onChange={onChange} tabs={tabs} />
         <PostCardsColumn
           posts={
             tabIndex === 0
@@ -17,7 +19,12 @@ const PostsPageBody = ({ tabIndex, onChange, tabs, posts }) => {
               : posts.filter((post) => post.categories.includes(tabs[tabIndex]))
           }
         />
-        <VerticalTabs className={'tabs'} value={tabIndex} onChange={onChange} tabs={tabs} />
+        <VerticalTabs
+          className={'vertical-tabs'}
+          value={tabIndex}
+          onChange={onChange}
+          tabs={tabs}
+        />
       </div>
     </div>
   );
