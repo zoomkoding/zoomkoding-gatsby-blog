02a00eb66aaa56835b6f01823dc4e2addbef9828
zoomkoding <zoomkoding@gmail.com>
2021-02-25 15:56:31 +0900

    chore: separate PostCard from PostCards

---
+++ src/components/post-card/index.js
@@ -0,0 +1,29 @@
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import './style.scss';
+
+const PostCard = ({ post }) => {
+  const { id, slug, title, excerpt, date, categories } = post;
+  return (
+    <div className="post-card-wrapper">
+      <Link className="post-card" key={id} to={slug}>
+        <div className="title">{title}</div>
+        <p className="description" dangerouslySetInnerHTML={{ __html: excerpt }} />
+        <div className="info">
+          <div className="date">{date}</div>
+          <div className="categories">
+            {categories.map((category) => (
+              <Link className="category" key={category} to={`/posts/${category}`}>
+                {category}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default PostCard;
