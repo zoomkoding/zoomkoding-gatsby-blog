ee00145b5ff0104ef14b1554cbd77ea8b2a0eb44
zoomkoding <zoomkoding@gmail.com>
2021-02-25 20:35:45 +0900

    feat: create VerticalTabs

---
+++ src/components/vertical-tabs/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Tabs, Tab } from '@material-ui/core';
+
+import './style.scss';
+
+export default function VerticalTabs({ value, onChange, tabs }) {
+  return (
+    <div className="vertical-tabs-wrapper">
+      <div className="vertical-tabs">
+        <Tabs
+          className="mui-tabs"
+          orientation="vertical"
+          value={value}
+          onChange={onChange}
+          variant="full-width"
+          scrollButtons="desktop"
+        >
+          {tabs.map((title, index) => (
+            <Tab label={title} key={index} />
+          ))}
+        </Tabs>
+      </div>
+    </div>
+  );
+}
