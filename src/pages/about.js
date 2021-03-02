478ccbc6e3fbc848df46a82db482d80454d7bb9a
zoomkoding <zoomkoding@gmail.com>
2021-03-03 02:56:14 +0900

    feat: create About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,28 +3,53 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Bio from '../components/bio';
+import TimeStampsSection from '../components/timestamps-section';
+import ProjectsSection from '../components/projects-section';
 
 export default ({ data }) => {
+  const metaData = data.site.siteMetadata;
+  const { timestamps, projects } = metaData.about;
   return (
     <Layout>
       <SEO title="About" />
       <Bio />
+      <TimeStampsSection timestamps={timestamps} />
+      <ProjectsSection projects={projects} />
     </Layout>
   );
 };
 
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
-      edges {
-        node {
-          id
-          html
-          frontmatter {
-            slug
-            categories
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        about {
+          timestamps {
+            date
+            activity
+            links {
+              post
+              github
+              demo
+              googlePlay
+              appStore
+            }
+          }
+          projects {
             title
-            date(formatString: "MMMM DD, YYYY")
+            description
+            techStack
+            thumbnailUrl
+            links {
+              post
+              github
+              demo
+              googlePlay
+              appStore
+            }
           }
         }
       }
