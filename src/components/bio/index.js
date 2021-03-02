0a80d9117c269ac7c7c63e7fdabe1502f134f509
zoomkoding <zoomkoding@gmail.com>
2021-03-03 02:53:54 +0900

    update: update Bio using Rotating Text

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -1,7 +1,7 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import authorVideo from '../../images/author.mp4'; //import the video file and then
-
+import ReactRotatingText from 'react-rotating-text';
 import './style.scss';
 
 const Bio = () => (
@@ -11,13 +11,17 @@ const Bio = () => (
         <p className="title">
           안녕하세요!
           <br />
-          배워서 남주는 개발자 <strong>정진혁</strong>입니다.
+          <ReactRotatingText
+            items={['배워서 남주고 싶은', '성장을 추구하는', '개발을 즐기는', '디자인도 관심있는']}
+          />
+          <br />
+          <strong>정진혁</strong>입니다.
           <br />
         </p>
-        <p className="description">
+        {/* <p className="description">
           오늘 내가 만들고 나누는 것이
           <br /> 누군가에게 도움을 줄 수 있다는 사실에서 동기를 얻습니다.
-        </p>
+        </p> */}
       </div>
 
       <video className="video" muted autoPlay loop>
