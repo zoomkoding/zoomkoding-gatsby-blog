2fd635e7440e0ebdfad4066c907db40acad530c4
zoomkoding <zoomkoding@gmail.com>
2021-03-03 16:06:35 +0900

    feat: add code styles using prismjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,4 @@ import './src/styles/_reset.scss';
 import './src/styles/_font.scss';
 import './src/styles/_global-style.scss';
 import './src/styles/_markdown-style.scss';
+import 'prismjs/themes/prism-tomorrow.css';
