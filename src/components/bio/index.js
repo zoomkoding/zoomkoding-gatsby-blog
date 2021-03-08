978f63d0a91b76f5d9ac275c64b7d6de65b8008c
zoomkoding <zoomkoding@gmail.com>
2021-03-08 22:45:20 +0900

    feat: create English bio

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -5,34 +5,61 @@ import ReactRotatingText from 'react-rotating-text';
 import './style.scss';
 import IconButtonBar from '../icon-button-bar';
 
-const Bio = ({ bio, social }) => (
-  <div className="bio-wrapper">
-    <div className="bio">
-      <div className="introduction">
-        <p className="title">
-          안녕하세요!
-          <br />
-          <ReactRotatingText items={bio?.description} />
-          <br />
-          개발자 <strong>{bio?.name}</strong>입니다.
-          <br />
-        </p>
-        <div className="social-links">
-          <IconButtonBar
-            links={social}
-            style={{
-              fontSize: '30px',
-              color: 'rgba(0, 0, 0, 0.54)',
-            }}
-          />
-        </div>
+const Bio = ({ bio, social }) => {
+  return (
+    <div className="bio-wrapper">
+      <div className="bio">
+        {bio?.language === 'ko' ? (
+          <div className="introduction korean">
+            <p className="title">
+              안녕하세요.
+              <br />
+              <ReactRotatingText items={bio?.description} />
+              <br />
+              개발자 <strong>{bio?.name}</strong>입니다.
+              <br />
+            </p>
+            <div className="social-links">
+              <IconButtonBar
+                links={social}
+                style={{
+                  fontSize: '30px',
+                  color: 'rgba(0, 0, 0, 0.54)',
+                }}
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="introduction english">
+            <p className="title">
+              Hello,
+              <br />
+              my name is
+              <br />
+              <strong>{bio?.name}</strong>
+              .<br />
+            </p>
+            <p className="description">
+              I'm a developer <ReactRotatingText items={bio?.description} />
+              <br />
+            </p>
+            <div className="social-links">
+              <IconButtonBar
+                links={social}
+                style={{
+                  fontSize: '30px',
+                  color: 'rgba(0, 0, 0, 0.54)',
+                }}
+              />
+            </div>
+          </div>
+        )}
+        <video className="video" muted autoPlay loop>
+          <source src={authorVideo} type="video/mp4" />
+        </video>
       </div>
-
-      <video className="video" muted autoPlay loop>
-        <source src={authorVideo} type="video/mp4" />
-      </video>
     </div>
-  </div>
-);
+  );
+};
 
 export default Bio;
