b718ff89c2f61e8508c2977b5a6656ddc6855860
zoomkoding <zoomkoding@gmail.com>
2021-08-15 11:58:16 +0900

    [#25] 이전과 다음글 표시 위치 변경

--- src/components/post-cards-adjacent/index.js
+++ src/components/post-cards-adjacent/index.js
@@ -7,18 +7,22 @@ const PostCardsAdjacent = ({ prevPost, nextPost }) => {
   return (
     <div className="post-cards-adjacent-wrapper">
       <div className="post-cards-adjacent">
-        {prevPost && (
-          <Link className="post-card prev" key={prevPost.id} to={prevPost.slug}>
-            <div className="direction">이전 글</div>
-            <div className="title">{prevPost.title}</div>
-          </Link>
-        )}
-        {nextPost && (
-          <Link className="post-card next" key={nextPost.id} to={nextPost.slug}>
-            <div className="direction">다음 글</div>
-            <div className="title">{nextPost.title}</div>
-          </Link>
-        )}
+        <div className="post-card-adjacent-wrapper">
+          {nextPost && (
+            <Link className="post-card prev" key={nextPost.id} to={nextPost.slug}>
+              <div className="direction">이전 글</div>
+              <div className="title">{nextPost.title}</div>
+            </Link>
+          )}
+        </div>
+        <div className="post-card-adjacent-wrapper">
+          {prevPost && (
+            <Link className="post-card next" key={prevPost.id} to={prevPost.slug}>
+              <div className="direction">다음 글</div>
+              <div className="title">{prevPost.title}</div>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
