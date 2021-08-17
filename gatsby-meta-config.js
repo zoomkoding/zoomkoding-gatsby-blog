d8cf6070b02eb575b71a1ec68a828e6eee236028
zoomkoding <zoomkoding@gmail.com>
2021-08-17 09:48:26 +0900

    meta에 links 잘못 들어간 부분 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -76,7 +76,30 @@ module.exports = {
         description:
           '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
         techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+        thumbnailUrl: 'zoomkoding.png',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo: 'https://www.zoomkoding.com',
+        },
+      },
+      {
+        title: '2020 우아한테크캠프 참여',
+        description:
+          '스타트업에서 앱 개발을 하면서 좋은 개발에 대한 갈증이 매우 크던 중에 좋은 기회를 잡게 되어 참여하게 되었습니다. 2달 간 몰입해서 좋은 사람들과 웹 개발을 경험하며, 많이 배우고 성장할 수 있었던 시간이었습니다.',
+        techStack: ['react', 'nodejs'],
+        thumbnailUrl: 'woowa-tech.png',
+        links: {
+          post: '/woowa-tech-camp-final',
+          github: 'https://github.com/woowa-techcamp-2020/bmart-6',
+        },
+      },
+      {
+        title: '대학시간(대학교 수강신청 보조 웹 앱) 개발',
+        description:
+          '학생들이 예비 수강 신청 과정에서 겪게 되는 불편함을 개선할 수 있는 웹 서비스를 개발하였습니다. 첫 서비스를 진행한 2021년 1학기에만 전체 학생의 절반 가량이 서비스를 경험했고, 사용한 학생들로부터 고맙다는 이야기를 여러 경로로 전달 받게 되었습니다. 개발이 줄 수 있는 좋은 영향력를 많이 느꼈던 프로젝트였습니다.',
+        techStack: ['react', 'nodejs'],
+        thumbnailUrl: 'timetable.png',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
           github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
