0e57df546de43d0fe58a3cab7fab328761ce8f37
zoomkoding <zoomkoding@gmail.com>
2021-08-31 00:44:29 +0900

    메타 정보 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -43,8 +43,57 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
+        date: '2018.07 ~ 2018.11',
+        activity: '인공지능 육목 알고리즘 개발 및 경진대회 우승',
+      },
+
+      {
+        date: '2019.06 ~ 2019.12',
+        activity: '알고리즘 휴학 및 코드포스 블루 달성',
+        links: {
+          post: '/how-to-algo',
+        },
+      },
+      {
+        date: '2019.12 ~ 2020.06',
+        activity: '스타트업 인턴십 진행 및 Picky 앱 개발',
+        links: {
+          post: '/start-up-app-development',
+          googlePlay: 'https://play.google.com/store/apps/details?id=care.jivaka.picky',
+          appStore: 'https://apps.apple.com/app/picky-skincare-made-smarter/id1504197356',
+        },
+      },
+
+      {
+        date: '2020.07 ~ 2020.08',
+        activity: '2020 우아한테크캠프 참여',
+        links: {
+          post: '/woowa-tech-camp-final',
+          github: 'https://github.com/woowa-techcamp-2020/bmart-6',
+        },
+      },
+
+      {
+        date: '2021.01 ~',
+        activity: '대학시간(대학교 수강신청 보조 웹 앱) 개발 및 운영',
+        links: {
+          post: '/college-timetable-development',
+          github: 'https://github.com/zoomkoding/college-timetable',
+          demo: 'https://www.timetable.college',
+        },
+      },
+      {
         date: '2021.02 ~',
         activity: '개인 블로그 개발 및 운영',
+        links: {
+          post: '/gatsby-starter-zoomkoding-introduction',
+          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo: 'https://www.zoomkoding.com',
+        },
+      },
+      {
+        date: '2021.04 ~',
+        activity: '만다오(프로모션 웹앱 빌더) 개발',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
           github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
@@ -72,26 +121,37 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
+        title: '만다오(프로모션 웹앱 빌더) 개발',
+        description:
+          '만다오는 우아한형제들에 입사한 후 처음으로 진행한 프로젝트로, 각종 프로모션 페이지를 마케터와 디자이너가 개발자 없이 만들 수 있게 하기 위한 WYSIWYG 방식의 웹 에디터입니다. 만다오 팀은 모든 사람이 디자인을 해야한다는 신념 하에 모든 팀원들이 직접 기획, 디자인, 개발을 진행하고 있습니다. 만다오는 제가 합류한 후에 고도화된 기능들이 많이 추가되어 가고 있고, 최종적으로는 모든 프로모션 페이지를 만들 수 있도록 계속해서 기능을 확장해나갈 예정입니다.',
+        techStack: ['react', 'nestjs'],
+        thumbnailUrl: 'mandao.png',
+        links: {
+          post: 'https://techblog.woowahan.com/2719',
+        },
+      },
+      {
+        title: 'Picky(글로벌 스킨케어 제품 분석 모바일 앱) 개발',
+        description:
+          '구글 출신 분들이 창업한 스타트업에 인턴십을 하던 중 피버팅을 하게 되면서 저는 1인 개발자로 신규 사업에 참여하게 되었습니다. 5명의 경험이 많은 팀원들과 힘을 합쳐 Picky라는 앱을 개발하고 지속적으로 서비스를 확장해나갔습니다. 이 과정을 통해 실제 서비스의 시작과 성장하는 과정을 경험해볼 수 있었습니다.',
+        techStack: ['flutter', 'nodejs'],
+        thumbnailUrl: 'picky.png',
+        links: {
+          post: '/start-up-app-development',
+          googlePlay: 'https://play.google.com/store/apps/details?id=care.jivaka.picky',
+          appStore: 'https://apps.apple.com/app/picky-skincare-made-smarter/id1504197356',
+        },
+      },
+      {
         title: '개발 블로그 테마 개발',
         description:
           '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
         techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'zoomkoding.png',
+        thumbnailUrl: 'blog.png',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
           github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
           demo: 'https://www.zoomkoding.com',
-        },
-      },
-      {
-        title: '2020 우아한테크캠프 참여',
-        description:
-          '스타트업에서 앱 개발을 하면서 좋은 개발에 대한 갈증이 매우 크던 중에 좋은 기회를 잡게 되어 참여하게 되었습니다. 2달 간 몰입해서 좋은 사람들과 웹 개발을 경험하며, 많이 배우고 성장할 수 있었던 시간이었습니다.',
-        techStack: ['react', 'nodejs'],
-        thumbnailUrl: 'woowa-tech.png',
-        links: {
-          post: '/woowa-tech-camp-final',
-          github: 'https://github.com/woowa-techcamp-2020/bmart-6',
         },
       },
       {
