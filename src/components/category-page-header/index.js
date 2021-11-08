11dd85ba32af9e2f8bfaddef36292d73434a5335
zoomkoding <zoomkoding@gmail.com>
2021-11-08 23:45:01 +0900

    카테고리 페이지 상단 텍스트 컴포넌트 이름 변경

--- src/components/category-page-header/index.js
+++ src/components/category-page-header/index.js
@@ -6,7 +6,7 @@ const PostsPageHeader = ({ title, subtitle }) => {
   return (
     <div className="posts-page-header-wrapper">
       <div className="posts-page-title">{title}</div>
-      <div className="posts-page-sub-title">{subtitle}</div>
+      <div className="posts-page-subtitle">{subtitle}</div>
     </div>
   );
 };
