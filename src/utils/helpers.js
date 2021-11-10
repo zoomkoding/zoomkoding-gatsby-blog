a485c3f06f14eb383915716c7827287fa186949b
zoomkoding <zoomkoding@gmail.com>
2021-11-10 18:54:10 +0900

    featured로 된 카테고리가 먼저 보여지도록 설정

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,10 @@
 const sortObjectByValue = (obj) => {
   let sortable = [];
   for (let item in obj) sortable.push([item, obj[item]]);
-  sortable.sort((a, b) => b[1] - a[1]);
+  sortable.sort((a, b) => {
+    if (a[0] === 'featured') return -1;
+    return b[1] - a[1];
+  });
   return sortable;
 };
 
