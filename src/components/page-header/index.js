906c4046ab1b9dacdf7776680185b3ead812d104
zoomkoding <zoomkoding@gmail.com>
2021-11-13 15:52:57 +0900

    [#46] 헤더에서 검색으로 페이지 목록을 내려주도록 변경

--- src/components/page-header/index.js
+++ src/components/page-header/index.js
@@ -1,28 +1,54 @@
-import { Link } from 'gatsby';
+import { Link, StaticQuery, graphql } from 'gatsby';
 import React from 'react';
+import Post from '../../models/post';
+import PostSearch from '../post-search';
 import ThemeSwitch from '../theme-switch';
 import './style.scss';
 
 function PageHeader({ siteTitle }) {
   return (
-    <header className="page-header-wrapper">
-      <div className="page-header">
-        <div className="front-section">
-          <Link className="link" to="/">
-            {siteTitle}
-          </Link>
-        </div>
-        <div className="trailing-section">
-          <Link className="link" to="/about">
-            about
-          </Link>
-          <Link className="link" to="/posts">
-            posts
-          </Link>
-          <ThemeSwitch />
-        </div>
-      </div>
-    </header>
+    <StaticQuery
+      query={graphql`
+        query SearchIndexQuery {
+          allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  categories
+                }
+                fields {
+                  slug
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data) => (
+        <header className="page-header-wrapper">
+          <div className="page-header">
+            <div className="front-section">
+              <Link className="link" to="/">
+                {siteTitle}
+              </Link>
+            </div>
+            <div className="trailing-section">
+              <Link className="link" to="/about">
+                about
+              </Link>
+              <Link className="link" to="/posts">
+                posts
+              </Link>
+              <PostSearch
+                posts={data.allMarkdownRemark.edges.map(({ node }) => new Post(node, true))}
+              />
+              <ThemeSwitch />
+            </div>
+          </div>
+        </header>
+      )}
+    />
   );
 }
 
